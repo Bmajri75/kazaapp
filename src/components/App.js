edf013ab5e7f041354b523b07413d5fa10b1e33e
Bechir Majris <bmajri@gmail.com>
2024-06-04 22:33:04 +0200

    mise en place du projet, next step, code

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Banner from "./Banner.js";
+
+function App() {
+  <div>
+    return <Banner />;
+  </div>;
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,6 @@
+import "../styles/Banner.css";
+
+function Banner() {
+  return <h1> TITLE </h1>;
+}
+export default Banner;
